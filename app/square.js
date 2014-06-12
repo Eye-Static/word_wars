f2d48fb71760b25d95e7b8d438c8d469ef7312ab
Zach <zachbowman0@gmail.com>
2014-06-12 11:21:06 -0700

    file structure and board generation

---
+++ app/square.js
@@ -0,0 +1,6 @@
+function Square (bonus)
+{
+  this.letter = null;  // each square starts with no letter on it
+  this.bonus = bonus;  // "st", "dl", "tl", "dw", "tw", ".."
+                       // ".." is a blank square
+}
