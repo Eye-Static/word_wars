6e33e68afccba2899788b4677a4efa58160e2cb9
Stephen Boles <stephenboles@gmail.com>
2014-06-18 12:50:19 -0700

    your commit message

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,16 @@
 'use strict';
 var Board = require('./Board');
+var Bag = require('./Bag');
 
 $(document).ready(function ()
 {
   var board = new Board();
+  var bag = new Bag();
 
   console.log(board);
 
   board.generate();  // create the board in data
   board.render();    // draw the board to html
+  bag.fill();
 });
+
