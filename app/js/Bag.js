7bb01d9eee6ea9f3f9ec3a3e8e67db5398cd850b
Zach <zachbowman0@gmail.com>
2014-06-18 13:10:57 -0700

    wrote shake and remove functions for bag

--- app/js/Bag.js
+++ app/js/Bag.js
@@ -16,9 +16,21 @@ module.exports = function Bag()
 
   //////////////////////////////////////////////////
 
-  // randomize the bag's letter array
-  this.shuffle = function ()
+  // Randomize the bag's letter array.
+  // I'm running through each letter in the array
+  // and swapping it with another, chosen at random.
+  this.shake = function ()
   {
+    var temp;  // for storing a letter to do the swap
+    var y;     // the random letter
+
+    for (var x = 0; x < this.letters.length; x += 1)
+    {
+      temp = this.letters[x];
+      y = Math.floor (Math.random() * this.letters.length);
+      this.letters[x] = this.letters[y];
+      this.letters[y] = temp;
+    }
   };
 
   //////////////////////////////////////////////////
@@ -26,5 +38,6 @@ module.exports = function Bag()
   // remove/pop the last letter object off the array and return it
   this.removeNext ()
   {
+    return this.letters.pop();
   };
 };
