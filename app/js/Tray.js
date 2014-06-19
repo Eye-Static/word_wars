b9f33b754ab8ee19b6a7a121a34c53de23b7f70b
Stephen Boles <stephenboles@gmail.com>
2014-06-19 10:47:13 -0700

    Letters on screen.

--- app/js/Tray.js
+++ app/js/Tray.js
@@ -1,5 +1,7 @@
 'use strict';
 var Letter = require('./Letter');
+var Bag = require('./Bag');
+var letterTemplate = require('./templates/letterTemplate.hbs');
 
 module.exports = function Tray ()
 {
@@ -9,8 +11,13 @@ module.exports = function Tray ()
 
   // remove letters from the bag and add them to the tray
   // until the tray has 7.
-  this.refill = function ()
+  this.refill = function (bag)
   {
+
+    //math randomize to pull tiles from the bag up to 7
+    while (this.letters.length<7) {
+      this.letters.push(bag.removeNext());
+    }  
   };
 
   //////////////////////////////////////////////////
@@ -25,5 +32,14 @@ module.exports = function Tray ()
   // remove one specific letter from the tray
   this.remove = function (letter)
   {
+  };
+    
+  this.render = function () {
+     $("#tray").empty();
+     for(var i=0; i<this.letters.length; i++){
+     $("#tray").append(letterTemplate(this.letters[i]));
+       
+    }
+   //$("#tray").append('string');
   };
 };
