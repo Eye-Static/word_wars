c730eed659fa43bd2512d9bca20edb1ff2b05b4e
Zach <zachbowman0@gmail.com>
2014-06-20 12:03:07 -0700

    reorganized main function, edited css, added button

--- app/js/app.js
+++ app/js/app.js
@@ -1,34 +1,35 @@
 'use strict';
 var Board  = require('./Board');
 var Bag    = require('./Bag');
-var Tray   = require('./Tray');
 var Player = require('./Player');
 
 $(document).ready(function ()
 {
-  var board = new Board('wordsWithFriends');
-  // grid layouts can be passed as strings like new Board('wordsWithFriends')
-
-  var bag = new Bag();
-  var player1 = new Player();
-
-  board.render();    // draw the board to html
-  console.log(board);
+  var board;
+  var bag;
+  var player = [];
 
   newGame();
 
-  player1.startTurn (bag);
+  console.log(board);
 
-  console.log(player1.tray.letters.length);
+  player[0].startTurn (bag);
+
+  console.log(player[0].tray.letters.length);
   console.log(bag.letters.length);
 
   //////////////////////////////////////////////////
 
   function newGame ()
   {
-    board = new Board();    // create the board in data
-    board.render();      // draw the board to html
+    // grid layouts can be passed as strings like new Board('wordsWithFriends')
+    board = new Board('wordsWithFriends');
+    bag = new Bag();
+    player[0] = new Player();
+    player[1] = new Player();
+
     bag.fill();          // add letters to bag
     bag.shake();         // randomize bag
+    board.render();      // draw the board to html
   }
 });
