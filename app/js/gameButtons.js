2d36d287423c509679883e055ed6fc50bb9889b6
Stephen Boles <stephenboles@gmail.com>
2014-06-26 13:41:59 -0700

    Dictionary first words

---
+++ app/js/gameButtons.js
@@ -0,0 +1,79 @@
+var validator = require('./validator');
+
+module.exports = function(game)
+{
+
+  $('#shuffle-tray-button').show();
+  $('#shuffle-tray-button').click (function ()
+  {
+    game.players[game.whoseTurn].tray.shuffle();
+    game.players[game.whoseTurn].tray.render();
+    //game.board.render (game.players[game.turn]);
+    game.board.addListeners (game.players);
+  });
+
+  //////////////////////////////////////////////////
+  $('#done-button').show();
+  $('#done-button').on('click', function()
+  {
+    if (validator.isValid(game))
+    {
+      game.finishTurn();
+      game.nextTurn();
+    }
+    // else
+    // {
+    //   alert ("Words must be placed in a solid straight line.");
+    // } 
+  })
+
+  //////////////////////////////////////////////////
+  $('#return-letters-button').show();
+  $('#return-letters-button').click (function ()
+  {
+    var player    = game.players[game.whoseTurn];
+    var boardGrid = game.board.grid;
+    var letter;
+
+    for(var y = 0; y < boardGrid.length; y ++)
+    {
+      for(var x = 0; x < boardGrid[y].length; x ++)
+      {
+        letter = boardGrid[y][x].letter;
+        if(letter && letter.justPlaced)
+        {
+          player.tray.letters.push(letter);
+          boardGrid[y][x].letter = null;
+        }
+      }
+    }
+    game.board.render();
+    player.tray.render();
+    game.board.addListeners(game.players);
+  });
+
+  //////////////////////////////////////////////////
+  // DEBUG BUTTONS //
+
+  $('#print-grid-button').click (function ()
+  {
+    game.board.printGrid();
+  });
+
+  //////////////////////////////////////////////////
+
+  $('#print-tray-button').click (function ()
+  {
+    for(var i = 0; i < game.players.length; i ++)
+    {
+      game.players[i].tray.print();
+    }
+  });
+
+  //////////////////////////////////////////////////
+
+  $('#print-placed-button').click (function ()
+  {
+    game.board.printPlaced();
+  });
+};
