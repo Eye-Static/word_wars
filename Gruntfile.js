1f71e14e9ca3e11d064204be949f1ca7e743241d
Zach <zachbowman0@gmail.com>
2014-07-30 10:44:59 -0700

    fixed green border

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,8 @@ module.exports = function(grunt) {
 
     concat: {
       dist: {
-        src: ['app/styles/*.css'],
+        src: ['app/styles/*.css', '!app/styles/signin.css'],
+        //src: ['app/styles/*.css'],
         dest: 'dist/style.css',
       },
     },
