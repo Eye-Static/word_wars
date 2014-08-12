a8a3a1b7bfb5cdf5c0fbd0721eb0c91de8ba8087
Zach <zachbowman0@gmail.com>
2014-08-12 13:32:11 -0700

    fixed score bug, edited boards, began mobile layout

--- app/js/grids.js
+++ app/js/grids.js
@@ -27,46 +27,46 @@ module.exports =
     },
   scrabble : {
     grid:
-    'XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX ' +
-    'XX XX TW .. .. .. DL .. .. TW .. .. DL .. .. .. TW XX XX ' +
-    'XX XX .. DW .. .. .. TL .. .. .. TL .. .. .. DW .. XX XX ' +
-    'XX XX .. .. DW .. .. .. DL .. DL .. .. .. DW .. .. XX XX ' +
-    'XX XX .. .. .. DW .. .. .. DL .. .. .. DW .. .. .. XX XX ' +
-    'XX XX DL .. .. .. DW .. TL .. TL .. DW .. .. .. DL XX XX ' +
-    'XX XX .. TL .. .. .. TL .. .. .. TL .. .. .. TL .. XX XX ' +
-    'XX XX .. .. DL .. DL .. DL .. DL .. DL .. DL .. .. XX XX ' +
-    'XX XX TW .. .. DL .. .. .. *  .. .. .. DL .. .. TW XX XX ' +
-    'XX XX .. .. DL .. DL .. DL .. DL .. DL .. DL .. .. XX XX ' +
-    'XX XX .. TL .. .. .. TL .. .. .. TL .. .. .. TL .. XX XX ' +
-    'XX XX DL .. .. .. DW .. DL .. DL .. DW .. .. .. DL XX XX ' +
-    'XX XX .. .. .. DW .. .. .. DL .. .. .. DW .. .. .. XX XX ' +
-    'XX XX .. .. DW .. .. .. DL .. DL .. .. .. DW .. .. XX XX ' +
-    'XX XX .. DW .. .. .. TL .. .. .. TL .. .. .. DW .. XX XX ' +
-    'XX XX TW .. .. .. DL .. .. TW .. .. DL .. .. .. TW XX XX ' +
-    'XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX',
-    width : 19,
+    'XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX ' +
+    'XX TW .. .. DL .. .. .. TW .. .. .. DL .. .. TW XX ' +
+    'XX .. DW .. .. .. TL .. .. .. TL .. .. .. DW .. XX ' +
+    'XX .. .. DW .. .. .. DL .. DL .. .. .. DW .. .. XX ' +
+    'XX DL .. .. DW .. .. .. DL .. .. .. DW .. .. DL XX ' +
+    'XX .. .. .. .. DW .. .. .. .. .. DW .. .. .. .. XX ' +
+    'XX .. TL .. .. .. TL .. .. .. TL .. .. .. TL .. XX ' +
+    'XX .. .. DL .. .. .. DL .. DL .. .. .. DL .. .. XX ' +
+    'XX TW .. .. DL .. .. .. *  .. .. .. DL .. .. TW XX ' +
+    'XX .. .. DL .. .. .. DL .. DL .. .. .. DL .. .. XX ' +
+    'XX .. TL .. .. .. TL .. .. .. TL .. .. .. TL .. XX ' +
+    'XX .. .. .. .. DW .. .. .. .. .. DW .. .. .. .. XX ' +
+    'XX DL .. .. DW .. .. .. DL .. .. .. DW .. .. DL XX ' +
+    'XX .. .. DW .. .. .. DL .. DL .. .. .. DW .. .. XX ' +
+    'XX .. DW .. .. .. TL .. .. .. TL .. .. .. DW .. XX ' +
+    'XX TW .. .. DL .. .. .. TW .. .. .. DL .. .. TW XX ' +
+    'XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX',
+    width : 17,
     height: 17
     },
   wordsWithFriends :{
     grid:
-    'XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX ' +
-    'XX XX .. .. .. TW .. .. TL .. TL .. .. TW .. .. .. XX XX ' +
-    'XX XX .. .. DL .. .. DW .. .. .. DW .. .. DL .. .. XX XX ' +
-    'XX XX .. DL .. .. DL .. .. .. .. .. DL .. .. DL .. XX XX ' +
-    'XX XX TW .. .. TL .. .. .. DW .. .. .. TL .. .. TW XX XX ' +
-    'XX XX .. .. DL .. .. .. DL .. DL .. .. .. DL .. .. XX XX ' +
-    'XX XX .. DW .. .. .. TL .. .. .. TL .. .. .. DW .. XX XX ' +
-    'XX XX TL .. .. .. DL .. .. .. .. .. DL .. .. .. TL XX XX ' +
-    'XX XX .. .. .. DW .. .. .. *  .. .. .. DW .. .. .. XX XX ' +
-    'XX XX TL .. .. .. DL .. .. .. .. .. DL .. .. .. TL XX XX ' +
-    'XX XX .. DW .. .. .. TL .. .. .. TL .. .. .. DW .. XX XX ' +
-    'XX XX .. .. DL .. .. .. DL .. DL .. .. .. DL .. .. XX XX ' +
-    'XX XX TW .. .. TL .. .. .. DW .. .. .. TL .. .. TW XX XX ' +
-    'XX XX .. DL .. .. DL .. .. .. .. .. DL .. .. DL .. XX XX ' +
-    'XX XX .. .. DL .. .. DW .. .. .. DW .. .. DL .. .. XX XX ' +
-    'XX XX .. .. .. TW .. .. TL .. TL .. .. TW .. .. .. XX XX ' +
-    'XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX',
-    width : 19,
+    'XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX ' +
+    'XX .. .. .. TW .. .. TL .. TL .. .. TW .. .. .. XX ' +
+    'XX .. .. DL .. .. DW .. .. .. DW .. .. DL .. .. XX ' +
+    'XX .. DL .. .. DL .. .. .. .. .. DL .. .. DL .. XX ' +
+    'XX TW .. .. TL .. .. .. DW .. .. .. TL .. .. TW XX ' +
+    'XX .. .. DL .. .. .. DL .. DL .. .. .. DL .. .. XX ' +
+    'XX .. DW .. .. .. TL .. .. .. TL .. .. .. DW .. XX ' +
+    'XX TL .. .. .. DL .. .. .. .. .. DL .. .. .. TL XX ' +
+    'XX .. .. .. DW .. .. .. *  .. .. .. DW .. .. .. XX ' +
+    'XX TL .. .. .. DL .. .. .. .. .. DL .. .. .. TL XX ' +
+    'XX .. DW .. .. .. TL .. .. .. TL .. .. .. DW .. XX ' +
+    'XX .. .. DL .. .. .. DL .. DL .. .. .. DL .. .. XX ' +
+    'XX TW .. .. TL .. .. .. DW .. .. .. TL .. .. TW XX ' +
+    'XX .. DL .. .. DL .. .. .. .. .. DL .. .. DL .. XX ' +
+    'XX .. .. DL .. .. DW .. .. .. DW .. .. DL .. .. XX ' +
+    'XX .. .. .. TW .. .. TL .. TL .. .. TW .. .. .. XX ' +
+    'XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX',
+    width : 17,
     height: 17
     },
 
